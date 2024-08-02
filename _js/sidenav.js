ea11998791d19e08fceff764c4dab2da8dd6be4d
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-08-02 19:38:30 -0300

    Update sidenav.js

--- _js/sidenav.js
+++ _js/sidenav.js
@@ -1,13 +1,13 @@
 function openNav() {
   document.getElementById("menu").style.width = "250px";
   document.getElementById("menu").style.border = "outset 3px wheat";
+  document.getElementById("overlayEscuro").style.display = "block";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "#500000";
 }
 
 function closeNav() {
   document.getElementById("menu").style.width = "0";
   document.getElementById("menu").style.border = "none";
+  document.getElementById("overlayEscuro").style.display = "none";
   document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "#860000";
 }
