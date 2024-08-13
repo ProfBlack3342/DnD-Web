6c73a50b817bd948f47be326f44ddc9c3a831c56
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-08-13 11:38:59 -0300

    Create tabelas.js

---
+++ _js/tabelas.js
@@ -0,0 +1,15 @@
+const livros = [];
+
+function preencherTabelaClasses() {
+    const classes = [];
+    const subClasses = [];
+    const tabela = [];
+}
+
+function preencherTabelaRacas() {
+    const racas = [];
+}
+
+function preencherTabelaBackgrounds() {
+    const backgrounds = [];
+}
