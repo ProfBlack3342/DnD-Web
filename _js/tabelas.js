b94c71072cdf617d2d741caf43bedea1069ef29f
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-08-13 11:52:52 -0300

    Update tabelas.js

--- _js/tabelas.js
+++ _js/tabelas.js
@@ -1,9 +1,12 @@
 const livros = [];
 
 function preencherTabelaClasses() {
-    const classes = [];
-    const subClasses = [];
-    const tabela = [];
+    const headerClasses = [];
+    const classes = listarClasses();
+    const subclasses = [];
+    for(i = 0; i < classes.length; i++) {
+        subclasses[i] = listarSubclasses(classes[i]);
+    }
 }
 
 function preencherTabelaRacas() {
@@ -12,4 +15,12 @@ function preencherTabelaRacas() {
 
 function preencherTabelaBackgrounds() {
     const backgrounds = [];
+}
+
+//Funções que acessam o banco de dados, ainda n implementadas
+function listarClasses() {
+
+}
+function listarSubclasses(classe) {
+
 }
